1f246992089206b1e8ea67ae2398d1c0a44fd94e
Ajith Abinash S <ajith@vgts.tech>
2022-03-07 15:03:01 +0530

    routing setup

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 // import MainLayout from "./components/layout/MainLayout";
 
@@ -6,11 +6,20 @@ import "antd/dist/antd.css";
 import Login from "./pages/login/Login";
 // import AuthenticatedRoute from "./components/wrapper/AuthenticatedRoute";
 import PageNotFound from "./pages/error 404/PageNotFound";
+import "./helper/axios.helper";
 import PageInfoProvider from "./contexts/PageInfoProvider";
-import Routing from './routing/Routing';
+import Routing from "./routing/Routing";
+import useMaster from "./hooks/useMasters";
 
 // import { authenticatedroutes, loginroutes } from "./routes/router";
 function App() {
+  const { fetchMasters } = useMaster();
+
+  useEffect(() => {
+    fetchMasters();
+    console.log("asmdkj");
+  }, []);
+
   return (
     <>
       <PageInfoProvider>
