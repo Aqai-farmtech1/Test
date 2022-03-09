b007459eb5e80e31bc5ad5131b4cb39b638dc8c8
Ajith Abinash <ajith@friendswebsolution.com>
2022-03-09 23:05:46 +0530

    m merge

--- src/pages/goat info/GoatWeightHistory.js
+++ src/pages/goat info/GoatWeightHistory.js
@@ -1,9 +1,9 @@
-import { Divider } from "antd";
+import { Divider, Skeleton } from "antd";
 import React from "react";
 import "./goatinfo.css";
 import GoatWeightHistoryCard from "./GoatWeightHistoryCard";
 
-export default function GoatWeightHistory({ goatData }) {
+export default function GoatWeightHistory({ goatData, isLoading }) {
   // console.log(goatData);
   return (
     <div className="goat_weight_history_main">
@@ -12,10 +12,77 @@ export default function GoatWeightHistory({ goatData }) {
         <Divider />
       </div>
       <div className="goat_weight_history_container">
-        {goatData.weight_history &&
-          goatData.weight_history.map((el) => (
-            <GoatWeightHistoryCard key={el.id} weightData={el} />
-          ))}
+        {isLoading ? (
+          <>
+            <div
+              className="skeleton_goat_weight_history"
+              style={{ display: "flex" }}
+            >
+              <Skeleton.Avatar
+                active
+                style={{ width: "70px", height: "70px" }}
+                size={"large"}
+              ></Skeleton.Avatar>
+              <Skeleton
+                active
+                style={{ marginLeft: "20px" }}
+                paragraph={{ rows: 2 }}
+              ></Skeleton>
+            </div>
+            <div
+              className="skeleton_goat_weight_history"
+              style={{ display: "flex" }}
+            >
+              <Skeleton.Avatar
+                active
+                style={{ width: "70px", height: "70px" }}
+                size={"large"}
+              ></Skeleton.Avatar>
+              <Skeleton
+                active
+                style={{ marginLeft: "20px" }}
+                paragraph={{ rows: 2 }}
+              ></Skeleton>
+            </div>
+            <div
+              className="skeleton_goat_weight_history"
+              style={{ display: "flex" }}
+            >
+              <Skeleton.Avatar
+                active
+                style={{ width: "70px", height: "70px" }}
+                size={"large"}
+              ></Skeleton.Avatar>
+              <Skeleton
+                active
+                style={{ marginLeft: "20px" }}
+                paragraph={{ rows: 2 }}
+              ></Skeleton>
+            </div>
+            <div
+              className="skeleton_goat_weight_history"
+              style={{ display: "flex" }}
+            >
+              <Skeleton.Avatar
+                active
+                style={{ width: "70px", height: "70px" }}
+                size={"large"}
+              ></Skeleton.Avatar>
+              <Skeleton
+                active
+                style={{ marginLeft: "20px" }}
+                paragraph={{ rows: 2 }}
+              ></Skeleton>
+            </div>
+          </>
+        ) : (
+          <>
+            {goatData.weight_history &&
+              goatData.weight_history.map((el) => (
+                <GoatWeightHistoryCard key={el.id} weightData={el} />
+              ))}
+          </>
+        )}
       </div>
     </div>
   );
